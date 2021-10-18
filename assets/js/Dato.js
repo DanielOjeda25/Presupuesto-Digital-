9ef2f6f09939568acc21d985ad932a5a4f6f7b16
Daniel Alejandro Ojeda <telorecomiendo01@gmail.com>
2021-10-18 10:43:51 -0300

    "clase Dato finalizada"

---
+++ assets/js/Dato.js
@@ -0,0 +1,19 @@
+//creamos la clase padre que tendra los datos
+class Dato {
+    constructor(descripcion, valor) {
+        this._descripcion = descripcion;
+        this._valor = valor;
+    }
+    get descripcion() {
+        return this._descripcion;
+    }
+    set descripcion(descripcion) {
+        return (this._descripcion = descripcion);
+    }
+    get valor() {
+        return this._valor;
+    }
+    set valor(valor) {
+        return (this._valor = valor);
+    }
+}
